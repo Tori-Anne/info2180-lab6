986d70bf37cdac35a07e1d3736fc86db377cd709
Tori-Anne <connell_tori@yahoo.com>
2018-11-12 13:53:33 -0500

    exercise 1 third commit

--- AJAX request.js
+++ AJAX request.js
@@ -5,6 +5,7 @@ function getRequest() {
 	request.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
 			document.getElementById("search").innerHTML = this.responseText;
+			alert("<?php echo $definition; ?>");
 		}
 	};
 	request.open('GET', 'request.php', true);
