5456e3e98915e41583b9b534b293eae76088e494
Tori-Anne <connell_tori@yahoo.com>
2018-11-12 13:28:52 -0500

    exercise 1 second commit

--- AJAX request.js
+++ AJAX request.js
@@ -1,10 +1,12 @@
 // Ajax request
 
 function getRequest() {
-	var request = XMLHttpRequest();
+	var request = new XMLHttpRequest();
 	request.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
-			document.getElementById("search").innerHTML = this.
+			document.getElementById("search").innerHTML = this.responseText;
+		}
+	};
 	request.open('GET', 'request.php', true);
 	request.send();
 }
