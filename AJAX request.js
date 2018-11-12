6905714974772fd61d5ffd86e65ce763fbc28ff8
Tori-Anne <connell_tori@yahoo.com>
2018-11-12 17:08:19 -0500

    exercise 3 fourth commit

--- AJAX request.js
+++ AJAX request.js
@@ -4,7 +4,8 @@ function getRequest() {
 	var request = new XMLHttpRequest();
 	request.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
-			document.getElementById("search").innerHTML = document.getElementById("result").value;
+			document.getElementById("search").innerHTML
+				// not working --> = document.getElementById("result").value;
 		}
 	};
 	request.open('GET', 'request.php', true);
