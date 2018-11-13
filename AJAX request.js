6299d306f68db4b91fc768382a035e89e6d72818
Tori-Anne <connell_tori@yahoo.com>
2018-11-13 01:13:37 -0500

    exercise 3 still not perfect

--- AJAX request.js
+++ AJAX request.js
@@ -1,14 +1,14 @@
 // Ajax request
 
 function getRequest() {
+	var word = document.getElementById("a_word");
 	var request = new XMLHttpRequest();
 	request.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
-			document.getElementById("search").innerHTML = this.responseText;
+			document.getElementById("result").innerHTML = this.responseText;
 		}
 	};
-	request.open('GET', 'request.php', true);
+	request.open('GET', 'request.php?q=' + word.value, true);
 	request.send();
 }
-
 
